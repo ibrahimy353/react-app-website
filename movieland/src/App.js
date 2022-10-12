47262ab23c16776957cbcc32a86791ca149d4657
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-10-12 23:10:45 +0300

    app.js edit

--- movieland/src/App.js
+++ movieland/src/App.js
@@ -5,11 +5,15 @@ const API_URL = "http://www.omdbapi.com?apikey=34e0b80c";
 const App =() => {
 
     const searchMovies = async (title) => { //async means it takes some times before it retrieves the data
-    const response = await fetch("${API_URL}&s=${title}");
+    const response = await fetch("${API_URL}&s=${title}"); //gets to call the API
+
     const data = await response.json();
+
+    console.log(data.Search);
 
     }
     useEffect(() => {
+        searchMovies('superman')
     }, {});
 
     return(
