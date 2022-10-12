ea38387314d1829b47ae8bdc8540a4a67f90af91
ibrahimy353 <85551204+ibrahimy353@users.noreply.github.com>
2022-10-12 23:08:07 +0300

    app.js editing

--- movieland/src/App.js
+++ movieland/src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import {useEffect} from "react";
 
+
+const API_URL = "http://www.omdbapi.com?apikey=34e0b80c";
 const App =() => {
+
+    const searchMovies = async (title) => { //async means it takes some times before it retrieves the data
+    const response = await fetch("${API_URL}&s=${title}");
+    const data = await response.json();
+
+    }
+    useEffect(() => {
+    }, {});
+
     return(
         <h1>app</h1>
     )
